1bdf146d86d7a7a78b9465ccfcf325b74f6943aa
htlee <hauten.lee@gmail.com>
2020-10-14 19:37:08 +0800

    Initialization

---
+++ SwiftMathEX/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftMathEX
+//
+//  Created by htlee on 2020/10/14.
+//  Copyright © 2020 HauTen Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
